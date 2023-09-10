9e8e362c734ec9629b38170299ff990729f40b93
kumar <ak1410mk@gmail.com>
2023-09-10 15:26:38 +0530

    commit

---
+++ portfolio/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Kumar</span> In the world of digital development and freelancing, Kumar stands as a shining example of excellence. Their versatile skill set, problem-solving prowess, commitment to quality, and client-centric approach make them the go-to choice for businesses and individuals seeking top-notch digital solutions. If you're looking to transform your ideas into reality or enhance your digital presence, [Developer's Name] is the trusted partner you need to bring your vision to life."
+      ],
+      typeSpeed: 100,
+      contentType: 'html'
+    });
+  }, 100);
